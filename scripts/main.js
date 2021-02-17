132fc83ebb52349cd017e8b87357f2930991b536
Hazel Phaho <Hazelphaho@gmail.com>
2021-02-17 23:39:02 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,31 @@
 
-var selectedColor = "color_1";
 var colorHex = "";
 
 //changes the wallpaper's background color
 function changeBg() {
 
-    //show the rgb label
-    document.getElementById('rgb-hex').style.display = "block";
 
-    //hide the gradient label
-    document.getElementById('gradient-hex-1').style.display = "none";
-    document.getElementById('gradient-hex-2').style.display = "none";
+    var r = parseInt(document.getElementById("red").value); //Red
+    var g = parseInt(document.getElementById("green").value); //Green
+    var b = parseInt(document.getElementById("blue").value); // Blue
+    var a = parseInt(document.getElementById("alpha").value); // Alpha
 
-    var r = parseInt(document.getElementById("red").value);
-    var g = parseInt(document.getElementById("green").value);
-    var b = parseInt(document.getElementById("blue").value);
+    //Alpha
+    a = calcApha(a);
 
-    var hex = "#" + getHex(r) + getHex(g) + getHex(b);
-    drawImage(hex);
-    document.getElementById('hex_color').innerHTML = hex;
+
+    //Get the hex value of red,green and blue colors
+    var hex = "#" + getHex(r) + getHex(g) + getHex(b) + a;
+
+    //change the body background to the hex value
+    document.body.style.background = hex;
+
+    //Update the hex global variable
     colorHex = hex;
 
-    document.getElementById('copyText').value = hex;
+    //Update the copy text value
+    document.getElementById('hex').value = `Hex:${hex}`;
+    //for debugging
     console.log(hex);
 
 }
@@ -37,126 +41,61 @@ function getHex(c) {
     return hex;
 }
 
-//draw an images, recieves color
-function drawImage(color) {
+function calcApha(a) {
+    a = Math.floor(a / 255 * 100)
+    if (a < 10) {
+        a = "0" + a;
+    }
 
-    var c = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
-    var w = canvas.width;
-    var h = canvas.height;
+    if (a > 99) {
+        a = "";
+    }
 
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, w, h);
-
-
+    return a;
 }
 
 
-function changeGradient() {
-
-    //hide the rgb label
-    document.getElementById('rgb-hex').style.display = "none";
-    //show the gradient label
-    document.getElementById('gradient-hex-1').style.display = "block";
-    document.getElementById('gradient-hex-2').style.display = "block";
-
-    var r = parseInt(document.getElementById("red_small").value);
-    var g = parseInt(document.getElementById("green_small").value);
-    var b = parseInt(document.getElementById("blue_small").value);
-
-
-    var hex = "#" + getHex(r) + getHex(g) + getHex(b);
-    document.getElementById(selectedColor).style.backgroundColor = hex;
-  
-    //update the labels
-    if(selectedColor=="color_1"){
-
-        document.getElementById('hex_color_1').innerHTML=hex;
-
-    }else if(selectedColor=="color_2"){
-
-        document.getElementById('hex_color_2').innerHTML=hex;
-    }
-
-    
-    document.getElementById('copyText').value = hex;
-    gradient();
-    console.log(hex);
-
+function shuffle_rgb() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var a = Math.floor(Math.random() * 255);
+    var new_Hex = "#" + getHex(r) + getHex(g) + getHex(b) + calcApha(a);
+    document.body.style.background = new_Hex;
+    //update the color sliders
+    document.getElementById("red").value = r; //Red
+    document.getElementById("green").value = g; //green
+    document.getElementById("blue").value = b; //blue
+    document.getElementById("alpha").value = a; //alpha
+    //Update the copy text value
+    document.getElementById('hex').value = `Hex:${new_Hex}`;
+    //update the hex global variable
+    colorHex = new_Hex;
 }
 
+var dice = document.getElementById('dice');
+dice.addEventListener('click', function () {
+    //shuffle the colors
+    shuffle_rgb();
+})
 
-function gradient() {
+//Copies hex val to clipboard
+document.getElementById('copy-icon').addEventListener('click', function () {
+    var text = document.getElementById('hex');
+    text.select();
+    text.setSelectionRange(0, 99999);
+    document.execCommand("copy");
 
-    var color_1 = document.getElementById('color_1').style.backgroundColor; //first color
-    var color_2 = document.getElementById('color_2').style.backgroundColor; //sencond color
-    var x = document.getElementById('grdientX-pos-slider').value; //slider value
-    
-    var c = document.getElementById('canvas'); 
-    var ctx = c.getContext('2d');
-    var w = canvas.width; //canvas width
-    var h = canvas.height; //canvas height
-    var startX = x/100*w;  //calulate the percentage
-    g = ctx.createLinearGradient(0, 0, startX, 0);
+    var tooltip = document.getElementById("mytoolTip");
+    tooltip.innerHTML = "Copied!";
+})
 
-    g.addColorStop(0, color_1);
-    g.addColorStop(1, color_2);
-
-    ctx.fillStyle = g;
-    ctx.fillRect(0, 0, w, h);
-
-    var perc = startX/w*100; // get percentage
-    //update the percentage
-    document.getElementById('gradient-pos-percentage').innerHTML = perc.toFixed(0)+"%";
-
-    console.log(w);
-
-}
-
-
-function switchColor(id) {
-
-    //determine with gradient color is selected
-    if (id == "color_1") {
-
-        selectedColor = "color_1"; //update selected color
-        document.getElementById('color_1').style.border = "2px solid cornflowerblue"
-        document.getElementById('color_2').style.border = "2px solid lightgrey"
-
-    }
-
-    if (id == "color_2") {
-        selectedColor = "color_2";
-        document.getElementById('color_2').style.border = "2px solid cornflowerblue"
-        document.getElementById('color_1').style.border = "2px solid lightgrey"
-    }
-
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
 }
 
 //init 
 function initialise() {
-    document.getElementById('color_1').style.backgroundColor = "#e66465";
-    document.getElementById('color_2').style.backgroundColor = "#9198e5";
-    document.getElementById('rgb-hex').style.display = "none";
-    document.getElementById('gradient-hex-1').style.display = "none";
-    document.getElementById('gradient-hex-2').style.display = "none";
-    document.getElementById('hex_color_1').innerHTML = "#e66465";
-    document.getElementById('hex_color_2').innerHTML = "#9198e5";
-}
-
-
-//copies hex values to clipboard
-function copyToclipBoard(id) {
-
-
-    var text = document.getElementById(id).innerText;
-    var textElement = document.getElementById('copyText')
-    textElement.style.display = "block";
-    textElement.value = text;
-    textElement.select();
-    document.execCommand("copy");
-    textElement.style.display = "none";
-    console.log(id);
-    console.log(text);
-
+    shuffle_rgb();
 }
